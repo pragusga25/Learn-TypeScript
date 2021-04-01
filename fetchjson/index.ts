e17a903144a8bdb6003abe1941b906d840a544ed
pragusga <taufik.pragusga@ui.ac.id>
2021-04-01 22:28:49 +0700

    Adding function

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -3,23 +3,26 @@ import axios from 'axios';
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
 interface Todo {
-    id: number;
-    title: string;
-    completed: boolean;
+  id: number;
+  title: string;
+  completed: boolean;
 }
 
-axios.get(url).then(res => {
-    const todo: Todo = res.data as Todo;
-    // const todo: Todo = res.data;
+axios.get(url).then((res) => {
+  const todo: Todo = res.data as Todo;
+  // const todo: Todo = res.data;
 
-    const ID = todo.id;
-    const title = todo.title;
-    const finished = todo.completed;
+  const { id } = todo;
+  const { title } = todo;
+  const { completed } = todo;
 
-    console.log(`
-        The Todo with ID: ${ID}
-        Has a little of: ${title}
-        Is it finished? ${finished}
-    `);
-    
+  logTodo(id, title, completed);
 });
+
+const logTodo = (id, title, completed) => {
+  console.log(`
+        The Todo with ID: ${id}
+        Has a little of: ${title}
+        Is it finished? ${completed}
+    `);
+};

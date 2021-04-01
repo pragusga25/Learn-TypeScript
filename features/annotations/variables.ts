716f97690e5feeded748f778c7abfd704775828a
pragusga <taufik.pragusga@ui.ac.id>
2021-04-01 22:48:40 +0700

    Creating features folder

---
+++ features/annotations/variables.ts
@@ -0,0 +1,12 @@
+let banana: number = 5;
+
+banana = 10;
+// banana = true; => will get an error
+
+let speed: string = 'fast';
+let hasName: boolean = true;
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+// built in objects
+let now: Date = new Date();

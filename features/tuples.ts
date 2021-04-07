2d49211a3c84daf9cf23f4817f7f76f1e7f6b13b
Taufik Pragusga <pragusga25@gmail.com>
2021-04-07 21:31:02 +0700

    Adding tupples with type annotations

---
+++ features/tuples.ts
@@ -0,0 +1,13 @@
+const cars = {
+  color: 'red',
+  price: 1900,
+  isNew: false,
+};
+
+const ferrari: [string, number, boolean] = ['red', 10000, true];
+// ferrari[1] = 'expensive';  => will get an error
+ferrari[1] = 20000;
+
+type Cars = [string, number, boolean];
+const lambo: Cars = ['green', 12000, false];
+const tesla: Cars = ['black', 8000, true];

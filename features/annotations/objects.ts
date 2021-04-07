41e79fa9d15a53dc2f9f16cd4eb47a7eb8242fa2
Taufik Pragusga <pragusga25@gmail.com>
2021-04-07 21:15:35 +0700

    Annotations around objects

---
+++ features/annotations/objects.ts
@@ -0,0 +1,21 @@
+const profilePerson = {
+  name: 'taufik',
+  age: 19,
+  coords: {
+    lat: 12,
+    lng: 20,
+  },
+  setAge(newAge: number): void {
+    this.age = newAge;
+  },
+};
+
+const { age }: { age: number } = profilePerson;
+const {
+  coords: { lat, lng },
+}: { coords: { lat: number; lng: number } } = profilePerson;
+
+console.log(age);
+console.log(lat);
+console.log(lng);
+// console.log(coords); => will get an error

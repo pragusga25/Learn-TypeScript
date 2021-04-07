3b2d802f7b0961543c6d5563b94edb30f2e7ee7f
Taufik Pragusga <pragusga25@gmail.com>
2021-04-07 21:47:12 +0700

    Adding more complex syntax in interfaces

--- features/interfaces.ts
+++ features/interfaces.ts
@@ -2,18 +2,22 @@ interface Car {
   name: string;
   price: number;
   isCool: boolean;
+  summary(): string;
 }
 
 const car = {
   name: 'tesla',
   price: 18000,
   isCool: true,
+  summary(): string {
+    return `Name: ${this.name}\n
+            Price: ${this.price}\n
+            Cool? ${this.isCool}`;
+  },
 };
 
 const printCar = (yourCar: Car): void => {
-  console.log(`Name: ${yourCar.name}`);
-  console.log(`Price: ${yourCar.price}`);
-  console.log(`Cool?: ${yourCar.isCool}`);
+  console.log(yourCar.summary());
 };
 
 printCar(car);

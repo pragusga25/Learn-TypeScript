355d71235b661f63dd881ac66b41e4eccc10380c
Taufik Pragusga <pragusga25@gmail.com>
2021-04-07 20:24:50 +0700

    Adding some reasons when to use annotations

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -40,7 +40,25 @@ const logName: (name: string) => void = (name: string) => {
 
 logName('Taufik Pragusga');
 
-// Any type
+// When to use annotations
+// 1) Function that returns the 'any' type
 const json = '{"yourName": "Taufik Pragusga", "age": 19}';
 const yourData: { yourName: string; age: number } = JSON.parse(json);
 console.log(yourData);
+
+// 2) When we declare a variable on one line
+// and initializate it later
+let words = ['red', 'green', 'white'];
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === 'green') foundWord = true;
+}
+
+// 3) Variable whose type cannot be inferred correctly
+let numbers = [-10, -2, 12];
+let numberAboveZero: boolean | number = false;
+
+for (let i = 0; i < numbers.length; i++) {
+  numberAboveZero = numbers[i];
+}

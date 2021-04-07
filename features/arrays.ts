2358f056be43561756bdbe4fce2bcd5eab15450b
Taufik Pragusga <pragusga25@gmail.com>
2021-04-07 21:24:09 +0700

    type annotation for arrays

---
+++ features/arrays.ts
@@ -0,0 +1,13 @@
+const cities: string[] = ['jakarta', 'bandung', 'bali'];
+const dates = [new Date(), new Date()];
+
+const carsByMake: string[][] = [
+  ['ferrari', 'bmw'],
+  ['tesla'],
+  ['lamborghini'],
+  ['honda'],
+];
+
+const mySchedule: (Date | string)[] = [new Date()];
+mySchedule.push('25-09-2001');
+mySchedule.push(new Date());

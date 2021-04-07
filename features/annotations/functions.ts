fd463b070dc9c3f9ebd57acb179509cb9315b6e7
Taufik Pragusga <pragusga25@gmail.com>
2021-04-07 20:59:22 +0700

    Destructuring around objects

--- features/annotations/functions.ts
+++ features/annotations/functions.ts
@@ -25,3 +25,26 @@ const logMessage = (message: string): void => {
 const throwError = (message: string): never => {
   throw new Error(message);
 };
+
+const forecast = {
+  date: new Date(),
+  weather: 'sunny',
+};
+
+// const logWeather = (forecast: { date: Date; weather: string }): void => {
+//   console.log(forecast.date);
+//   console.log(forecast.weather);
+// };
+
+const logWeather = ({
+  date,
+  weather,
+}: {
+  date: Date;
+  weather: string;
+}): void => {
+  console.log(date);
+  console.log(weather);
+};
+
+logWeather(forecast);

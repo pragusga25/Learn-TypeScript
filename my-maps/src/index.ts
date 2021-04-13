0a7c53717962a02e2f991687103591316507b595
Taufik Pragusga <pragusga25@gmail.com>
2021-04-13 16:07:32 +0700

    Adding code to show the map

--- my-maps/src/index.ts
+++ my-maps/src/index.ts
@@ -1,8 +1,11 @@
-import { Company } from './Company';
-import { User } from './User';
+// import { Company } from './Company';
+// import { User } from './User';
 
-const user = new User();
-const company = new Company();
-
-console.log(user);
-console.log(company);
+new google.maps.Map(document.getElementById('map'), {
+  zoom: 1,
+  backgroundColor: 'black',
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});

543ebdae3e31a69e0d9093367ddc6cb60b4b5f77
Taufik Pragusga <pragusga25@gmail.com>
2021-04-13 16:15:09 +0700

    Adding CustomMap.ts file

--- my-maps/src/index.ts
+++ my-maps/src/index.ts
@@ -1,11 +1,5 @@
+import { CustomMap } from './CustomMap';
 // import { Company } from './Company';
 // import { User } from './User';
 
-new google.maps.Map(document.getElementById('map'), {
-  zoom: 1,
-  backgroundColor: 'black',
-  center: {
-    lat: 0,
-    lng: 0,
-  },
-});
+new CustomMap();

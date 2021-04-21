b911f0217551b4ebac25d809f09455691144a912
Taufik Pragusga <pragusga25@gmail.com>
2021-04-21 23:25:09 +0700

    Showing popup windows when clicked the marker

--- my-maps/src/CustomMap.ts
+++ my-maps/src/CustomMap.ts
@@ -22,22 +22,20 @@ export class CustomMap {
   }
 
   addMarker(mappable: Mappable): void {
-    new google.maps.Marker({
+    const marker = new google.maps.Marker({
       map: this.googleMap,
       position: {
         lat: mappable.location.lat,
         lng: mappable.location.lng,
       },
     });
-  }
 
-  // addCompanyMarker(company: Company): void {
-  //   new google.maps.Marker({
-  //     map: this.googleMap,
-  //     position: {
-  //       lat: company.location.lat,
-  //       lng: company.location.lng,
-  //     },
-  //   });
-  // }
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: 'Hi There!',
+      });
+
+      infoWindow.open(this.googleMap, marker);
+    });
+  }
 }

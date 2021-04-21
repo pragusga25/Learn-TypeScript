bea5f2a90f15b647e333e9322724f4ad1466e482
Taufik Pragusga <pragusga25@gmail.com>
2021-04-21 23:06:17 +0700

    Deleting addCompanyMarker function and creating a more generic function to create marker

--- my-maps/src/index.ts
+++ my-maps/src/index.ts
@@ -4,5 +4,7 @@ import { User } from './User';
 
 const user = new User();
 const company = new Company();
+const map = new CustomMap('map');
 
-new CustomMap('map').addUserMarker(user);
+map.addMarker(user);
+map.addMarker(company);

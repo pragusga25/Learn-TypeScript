b4af394ca0183e1cd683c3dcd225ae2b73a31288
Taufik Pragusga <pragusga25@gmail.com>
2021-04-22 23:25:03 +0700

    Adding some files, creating NumbersColletion class, and separating swapping and comparison

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,19 +1,7 @@
-class Sorter {
-  constructor(public collection: number[]) {}
+import { NumbersCollection } from './NumbersCollection';
+import { Sorter } from './Sorter';
 
-  sort = (): void => {
-    const { length } = this.collection;
-
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection[j] > this.collection[j + 1]) {
-          const leftHand = this.collection[j];
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = leftHand;
-        }
-      }
-    }
-  };
-}
-
-const sorter = new Sorter([10, -1, 4, 6, 2]);
+const numbersCollection = new NumbersCollection([10, -1, 0, -2, 12, 6]);
+const sorter = new Sorter(numbersCollection);
+sorter.sort();
+console.log(numbersCollection.data);

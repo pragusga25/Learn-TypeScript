4cab102678615a5808792259a65f9c462fafc2aa
Taufik Pragusga <pragusga25@gmail.com>
2021-04-23 23:21:44 +0700

    Sorting arbitrary Collections

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,7 +1,13 @@
-import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
+// import { NumbersCollection } from './NumbersCollection';
 import { Sorter } from './Sorter';
 
-const numbersCollection = new NumbersCollection([10, -1, 0, -2, 12, 6]);
-const sorter = new Sorter(numbersCollection);
+// const numbersCollection = new NumbersCollection([10, -1, 0, -2, 12, 6]);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection('AaXZys');
+const sorter = new Sorter(charactersCollection);
 sorter.sort();
-console.log(numbersCollection.data);
+console.log(charactersCollection.data);

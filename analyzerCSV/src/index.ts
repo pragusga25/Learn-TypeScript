381064b292a9d58bbe93fc3c621c9680d2b6ccec
Taufik Pragusga <pragusga25@gmail.com>
2021-04-26 13:31:21 +0700

    Using enums

--- analyzerCSV/src/index.ts
+++ analyzerCSV/src/index.ts
@@ -7,11 +7,18 @@ const matches = fs
   .split('\n')
   .map((row: string): string[] => row.split(','));
 
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D',
+}
+
 let manUnitedWins = 0;
 
 for (let match of matches) {
-  match[1] === 'Man United' && match[5] === 'H' && manUnitedWins++;
-  match[2] === 'Man United' && match[5] === 'A' && manUnitedWins++;
+  ((match[1] === 'Man United' && match[5] === MatchResult.HomeWin) ||
+    (match[2] === 'Man United' && match[5] === MatchResult.AwayWin)) &&
+    manUnitedWins++;
 }
 
 console.log(`Man United won ${manUnitedWins} games`);

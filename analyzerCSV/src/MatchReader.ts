604d4b62870a7cebaa33d8f9c52d6275c1ae9399
Taufik Pragusga <pragusga25@gmail.com>
2021-04-28 17:25:59 +0700

    Refactoring the code to inheritance [Without abstract class]

--- analyzerCSV/src/MatchReader.ts
+++ analyzerCSV/src/MatchReader.ts
@@ -1,19 +1,8 @@
-import { CsvReader } from './CsvReader';
-import { MatchResult } from './MatchResult';
-import { dateStringToDate } from './utils';
+interface DataReader {
+  read(): void;
+  data: string[][];
+}
 
-type MatchData = [Date, string, string, number, number, MatchResult, string];
-
-export class MatchReader extends CsvReader<MatchData> {
-  mapRow(row: string[]): MatchData {
-    return [
-      dateStringToDate(row[0]),
-      row[1],
-      row[2],
-      parseInt(row[3]),
-      parseInt(row[4]),
-      row[5] as MatchResult,
-      row[6],
-    ];
-  }
+export class MatchReader {
+  constructor(public reader: DataReader) {}
 }

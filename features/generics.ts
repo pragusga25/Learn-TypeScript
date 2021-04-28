132cfc1fab0c65e52f7a5d2596e7c946b0123e11
Taufik Pragusga <pragusga25@gmail.com>
2021-04-28 19:31:34 +0700

    Type inference with generics and function generics

---
+++ features/generics.ts
@@ -0,0 +1,51 @@
+// Example of generics with functions
+class ArrayOfNumbers {
+  constructor(public collection: number[]) {}
+
+  get(index: number): number {
+    return this.collection[index];
+  }
+}
+
+class ArrayOfStrings {
+  constructor(public collection: string[]) {}
+
+  get(index: number): string {
+    return this.collection[index];
+  }
+}
+
+// class with generics
+class ArrayOfAnything<T> {
+  constructor(public collection: T[]) {}
+
+  get(index: number): T {
+    return this.collection[index];
+  }
+}
+
+const arrayOfNumbers = new ArrayOfAnything<number>([1, 2, 3, 4, 5]);
+const arrayOfStrings = new ArrayOfAnything<string>(['a', 'b', 'c']);
+
+// Example of generics with functions
+function printStrings(arr: string[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+function printNumbers(arr: number[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+// Generics function
+function printAnything<T>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+printAnything<number>([1, 2, 3, 4, 5]);
+printAnything<string>(['a', 'b', 'c', 'd']);

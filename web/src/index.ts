6b1949b5e830ec37985cda5b4ac43ed32d140cc2
Taufik Pragusga <pragusga25@gmail.com>
2021-04-29 12:35:02 +0700

    Adding fetch functionality

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,23 +1,3 @@
-import { User } from './models/User';
+import axios from 'axios';
 
-const user = new User({ name: 'Taufik Pragusga', age: 19 });
-
-user.on('change', () => {
-  console.log('Hello, I am in change event 1');
-});
-
-user.on('change', () => {
-  console.log('Hello, I am in change event 2');
-});
-
-user.trigger('change');
-
-console.log(user);
-
-// console.log(user.get('name'));
-// console.log(user.get('age'));
-
-// user.set({ name: 'Pragusga' });
-
-// console.log(user.get('name'));
-// console.log(user.get('age'));
+axios.get('http://localhost:3000/users/1');

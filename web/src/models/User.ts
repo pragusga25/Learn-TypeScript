644ff602b3ef33c5e6040b401d74a7262b93d317
Taufik Pragusga <pragusga25@gmail.com>
2021-04-29 12:48:02 +0700

    Saving user data

--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -30,7 +30,7 @@ export class User {
   trigger(eventName: string): void {
     const handlers = this.events[eventName];
 
-    if (!handlers) return;
+    if (!handlers || handlers.length === 0) return;
 
     handlers.forEach((callback) => callback());
   }
@@ -41,5 +41,15 @@ export class User {
       .then((response: AxiosResponse): void => {
         this.set(response.data);
       });
+  }
+
+  save(): void {
+    const id = this.get('id');
+
+    if (id) {
+      axios.put(`http://localhost:3000/users/${id}`, this.data);
+    } else {
+      axios.post('http://localhost:3000/users', this.data);
+    }
   }
 }

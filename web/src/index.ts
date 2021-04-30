a92e22e2d1e5fef58005d6b1e6f889b92b8109db
Taufik Pragusga <pragusga25@gmail.com>
2021-04-30 17:20:58 +0700

    Refactoring with composition

--- web/src/index.ts
+++ web/src/index.ts
@@ -2,4 +2,10 @@ import { User } from './models/User';
 
 const user = new User({ name: 'John', age: 99 });
 
+user.events.on('change', () => {
+  console.log('From event');
+});
+
+user.events.trigger('change');
+
 user.save();

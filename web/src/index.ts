cf429180701e92d1eeffabdf49e03c9f22580602
Taufik Pragusga <pragusga25@gmail.com>
2021-05-01 05:15:54 +0700

    Passthrough methods

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,11 +1,5 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'John', age: 99 });
+const user = new User({ name: 'John in The House', age: 99 });
 
-user.events.on('change', () => {
-  console.log('From event');
-});
-
-user.events.trigger('change');
-
-user.save();
+user.sync.save();

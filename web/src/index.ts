6fc7df0444c92a119f8128c28e3126ea9b020903
Taufik Pragusga <pragusga25@gmail.com>
2021-05-01 22:15:21 +0700

    Setting data while triggering

--- web/src/index.ts
+++ web/src/index.ts
@@ -3,3 +3,9 @@ import { User } from './models/User';
 const user = new User({ name: 'John in The House', age: 99 });
 
 console.log(user.get('name'));
+
+user.on('change', () => {
+  console.log('User was changed');
+});
+
+user.set({ name: 'Pragusga Taufik' });

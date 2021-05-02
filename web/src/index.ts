85eacb21b26cf726cf8209f9792f761692e865e6
Taufik Pragusga <pragusga25@gmail.com>
2021-05-03 06:46:26 +0700

    Strict null checks

--- web/src/index.ts
+++ web/src/index.ts
@@ -3,5 +3,10 @@ import { UserForm } from './views/UserForm';
 
 const user = User.buildUser({ name: 'BOCILLLL', age: 7 });
 
-const userForm = new UserForm(document.getElementById('root'), user);
-userForm.render();
+const root = document.getElementById('root');
+if (root) {
+  const userForm = new UserForm(root, user);
+  userForm.render();
+} else {
+  throw new Error('Root element not found');
+}
--- web/src/views/UserForm.ts
+++ web/src/views/UserForm.ts
@@ -29,9 +29,10 @@ export class UserForm {
   onSetNameClick = (): void => {
     const input = this.parent.querySelector('input');
 
-    const name = input.value;
-
-    this.model.set({ name });
+    if (input) {
+      const name = input.value;
+      this.model.set({ name });
+    }
   };
 
   template(): string {

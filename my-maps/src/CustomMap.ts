d839ebf2f124463e9044d01bca5243437b14cd6c
Taufik Pragusga <73622464+pragusga25@users.noreply.github.com>
2021-05-03 06:16:26 +0700

    FINISH

--- my-maps/src/CustomMap.ts
+++ my-maps/src/CustomMap.ts
@@ -31,7 +31,7 @@ export class CustomMap {
       },
     });
 
-    marker.addListener('click', () => {
+    marker.addListener('click', ():void => {
       const infoWindow = new google.maps.InfoWindow({
         content: mappable.markerContent(),
       });

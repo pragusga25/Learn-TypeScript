7332c2100b938e5cde694450fd02f34309fa29c0
Taufik Pragusga <pragusga25@gmail.com>
2021-05-02 21:16:21 +0700

    Parsing user json

--- web/src/models/Collection.ts
+++ web/src/models/Collection.ts
@@ -1,11 +1,29 @@
+import axios, { AxiosResponse } from 'axios';
 import { Eventing } from './Eventing';
-import { User } from './User';
+import { User, UserProps } from './User';
 
 export class Collection {
   models: User[] = [];
   events: Eventing = new Eventing();
 
+  constructor(public rootUrl: string) {}
+
   get on() {
     return this.events.on;
+  }
+
+  get trigger() {
+    return this.events.trigger;
+  }
+
+  fetch(): void {
+    axios.get(this.rootUrl).then((res: AxiosResponse): void => {
+      res.data.forEach((value: UserProps): void => {
+        const user = User.buildUser(value);
+        this.models.push(user);
+      });
+
+      this.trigger('change');
+    });
   }
 }

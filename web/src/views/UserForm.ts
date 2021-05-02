a1658e5c30ea297b19181c0cb5ae2e785011bc44
Taufik Pragusga <pragusga25@gmail.com>
2021-05-03 06:35:08 +0700

    Re-rendering on Model change

--- web/src/views/UserForm.ts
+++ web/src/views/UserForm.ts
@@ -5,7 +5,15 @@ interface EventsMap {
 }
 
 export class UserForm {
-  constructor(public parent: Element, public model: User) {}
+  constructor(public parent: Element, public model: User) {
+    this.bindModel();
+  }
+
+  bindModel(): void {
+    this.model.on('change', () => {
+      this.render();
+    });
+  }
 
   eventsMap(): EventsMap {
     return {
@@ -43,6 +51,8 @@ export class UserForm {
   }
 
   render(): void {
+    this.parent.innerHTML = '';
+
     const templateElement = document.createElement('template');
     templateElement.innerHTML = this.template();
 

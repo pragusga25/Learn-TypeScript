9eeb2fa84fc851b5b74e8c7f6027b12efd78cb23
Taufik Pragusga <pragusga25@gmail.com>
2021-05-03 04:36:44 +0700

    Generic User Collection

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,6 +1,10 @@
 import { Collection } from './models/Collection';
+import { User, UserProps } from './models/User';
 
-const collection = new Collection('http://localhost:3000/users');
+const collection = new Collection<User, UserProps>(
+  (json: UserProps) => User.buildUser(json),
+  'http://localhost:3000/users'
+);
 
 collection.on('change', () => {
   console.log(collection);

4ab6e39aeb8ba34534136fa3f65bd3c5771ba529
Taufik Pragusga <pragusga25@gmail.com>
2021-05-14 21:43:30 +0700

    Testing Region Mapping

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,12 +1,14 @@
+import { UserEdit } from './views/UserEdit';
 import { User } from './models/User';
-import { UserForm } from './views/UserForm';
 
 const user = User.buildUser({ name: 'BOCILLLL', age: 7 });
 
 const root = document.getElementById('root');
 if (root) {
-  const userForm = new UserForm(root, user);
-  userForm.render();
+  const userEdit = new UserEdit(root, user);
+  userEdit.render();
+
+  console.log(UserEdit);
 } else {
   throw new Error('Root element not found');
 }

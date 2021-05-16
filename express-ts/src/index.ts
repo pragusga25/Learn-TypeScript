055851b48bf875036215b919162f076d3b2d5dc3
Taufik Pragusga <pragusga25@gmail.com>
2021-05-16 22:15:52 +0700

    Creating a boilerplate for index.ts

--- express-ts/src/index.ts
+++ express-ts/src/index.ts
@@ -1,1 +1,17 @@
-console.log('Hello World');
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req: Request, res: Response) => {
+  res.send(
+    `
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    `
+  );
+});
+
+app.listen(6789, () => console.log('Server is running on port 6789'));
